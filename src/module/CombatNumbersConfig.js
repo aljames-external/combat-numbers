b0ddb519c78dc489bc3e608aa874ac0c5f12eb04
Alexander James <aljames@google.com>
2026-07-04 00:01:44 +0000

    fix(config): restore DEFAULT_APPEARANCE static getters on CombatNumbersConfig

--- src/module/CombatNumbersConfig.js
+++ src/module/CombatNumbersConfig.js
@@ -13,6 +13,63 @@ export default class CombatNumbersConfig extends FormApplication {
     });
   }
 
+  /**
+   * The default appearance settings object.
+   *
+   * @return {Object}
+   */
+  static get DEFAULT_APPEARANCE() {
+    return {
+      font: 'Verdana',
+      fontSize: 'medium',
+      bold: true,
+      italic: false,
+      damageColor: '#ffffff',
+      healColor: '#95ed98',
+      strokeColor: '#000000',
+      strokeThickness: 5,
+      dropShadowColor: '#000000',
+      dropShadowAlpha: 1,
+    };
+  }
+
+  /**
+   * The built-in font families to choose from.
+   *
+   * @return {array}
+   */
+  static get FONT_FAMILIES() {
+    return [
+      'Verdana',
+      'Arial',
+      'Helvetica',
+      'Trebuchet MS',
+      'Times New Roman',
+      'Modesto',
+      'Didot',
+      'American Typewriter',
+      'Andale Mono',
+      'Courier',
+      'Bradley Hand',
+      'Luminari',
+    ];
+  }
+
+  /**
+   * The available font sizes to choose from.
+   *
+   * @return {Object}
+   */
+  static get FONT_SIZES() {
+    return {
+      xsmall: 'Extra Small',
+      small: 'Small',
+      medium: 'Medium',
+      large: 'Large',
+      xlarge: 'Extra Large',
+    };
+  }
+
   async getData(options) {
     const system = game.system;
     return {
